0eb7b6d797ebd359d4bb1217fab5b2306a519569
wangweiya <19309695378@163.com>
2023-02-21 14:47:11 +0800

    less

--- src/pages/Login/login.vue
+++ src/pages/Login/login.vue
@@ -1,6 +1,19 @@
-<template >
-    <div>
-        <h1>登录页</h1>
-    </div>
+<template>
+  <div>
+    <input type="text">
+    <input type="text">
+  </div>
 </template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="less" scoped>
+div{
+  input{
+    width: 20em;
+  }
+}
+</style>
     
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="vite/client" />
+// declare module 'axios'
+declare module 'element-plus'
