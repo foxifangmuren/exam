8ae8054a20704d33b3afa1381f9f9d60f76a6ac7
wangweiya <19309695378@163.com>
2023-02-21 16:03:25 +0800

    123

--- src/pages/Login/login.vue
+++ src/pages/Login/login.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
-
+    <input type="text" v-model="params.usename" />
+    <input type="text" v-model="params.pass" />
+    <button @click="log">登录</button>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { login } from '../../api/admin';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const params = {
+  usename: 'admin',
+  pass: 'admin',
+};
+const log = async () => {
+  const res: any = await login(params.usename, params.pass);
+  console.log(res);
+  sessionStorage.setItem('token', res.data);
+  router.push('/Home');
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="less" scoped>
+div {
+  input {
+    width: 20em;
+  }
+}
 </style>
