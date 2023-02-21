f8a1d74c7da8ce497602d69f34302b2463461c91
zhang <3413640887@qq.com>
2023-02-21 15:19:58 +0800

    1

--- src/pages/Login/login.vue
+++ src/pages/Login/login.vue
@@ -1,23 +1,2 @@
-<template>
-    <div class="loginbox">
-        <div class="box">
-          <div class="log">
-            <img src="../../img/" alt="">
-          </div>
-        </div>
-    </div>
-</template>
-<script lang="ts" setup></script>
-<style lang="less" scoped>
-  body{
-    margin: 0;
-    padding: 0;
-    .box{
-      width: 535px;
-      height: 630px;
-      background-color: #ffffff;
-      box-shadow: 4px 4px 4px 4px #ccc;
-    }
-  }
-</style>
+
     
