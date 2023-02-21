0b3be9734b1c40e91813f2ae21bc9083d9f75fcc
zhangyirui123 <2979281910@qq.com>
2023-02-21 20:26:32 +0800

    张瑞振提交阅卷布局

--- src/pages/Home/children/Exam/Exam.vue
+++ src/pages/Home/children/Exam/Exam.vue
@@ -1,13 +1,100 @@
 <template>
   <div>
-1323
+    <!-- 头部 -->
+    <div>
+      <p class="titile_header">阅卷管理</p>
+      <div class="title_box_input">
+        <p>关键字:</p>
+        <el-input
+          class="titl_input"
+          v-model="input"
+          placeholder="Please input"
+          clearable
+        />
+        <el-button type="primary">Primary</el-button>
+      </div>
+    </div>
+    <!-- 表格 -->
+    <el-table :data="tableData" style="width: 100%" :header-cell-style="{background:'#f5f7fa'}">
+      <el-table-column prop="date" label="Date" />
+      <el-table-column prop="name" label="Name" />
+      <el-table-column label="Address" width="180">
+        <el-button key="primary" type="primary" link>阅卷</el-button>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <div class="demo-pagination-block">
+      <el-pagination
+        v-model:current-page="currentPage4"
+        v-model:page-size="pageSize4"
+        :page-sizes="[100, 200, 300, 400]"
+        :small="small"
+        :disabled="disabled"
+        :background="background"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import { ref } from "vue";
+const input = ref("");
+const tableData = [
+  {
+    date: "2016-05-03",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-02",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-04",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-01",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+];
 
+const currentPage4 = ref(4);
+const pageSize4 = ref(100);
+const small = ref(false);
+const background = ref(false);
+const disabled = ref(false);
+
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`);
+};
+const handleCurrentChange = (val: number) => {
+  console.log(`current page: ${val}`);
+};
 </script>
-
-<style lang="scss" scoped>
-
+<style scoped>
+.titile_header{
+    font-size: 20px;
+}
+.title_box_input {
+  display: flex;
+  align-items: center;
+}
+.titl_input {
+  width: 200px;
+  margin: 0 30px 0 10px;
+}
+.demo-pagination-block {
+  width: 600px;
+  margin: 20px auto;
+}
 </style>
+
+    
