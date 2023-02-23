49b8f5eea1e16f6ed2a40726b392c078fdd0d157
刘子诺 <lzn13832092800@163.com>
2023-02-23 15:28:32 +0800

    删除

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -34,17 +34,25 @@ export const classesadd = (params: any): Promise<AxiosResponse<any>> => {
 export const classesdel = (params: any): Promise<AxiosResponse<any>> => {
   return get('/classes/delete',params)
 }
-//学生列表 
+//学员列表 
 export const studentlist = (params: any): Promise<AxiosResponse<any>> => {
   return get('/student/list',params)
 }
-//删除学生
+//删除学员
 export const studentdel = (params: any): Promise<AxiosResponse<any>> => {
   return get('/student/delete',params)
+}
+//批量删除学员
+export const studentdelall = (params: any): Promise<AxiosResponse<any>> => {
+  return post('/student/deleteall',params)
 }
 //角色管理列表
 export const rolelist = (params: any): Promise<AxiosResponse<any>> => {
   return get('/role/list',params)
+}
+//角色删除
+export const roledel = (params: any): Promise<AxiosResponse<any>> => {
+  return get('/role/delete',params)
 }
 //师资列表
 export const teacherlist = (params: any): Promise<AxiosResponse<any>> => {
