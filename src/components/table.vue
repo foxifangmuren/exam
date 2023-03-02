46bb6acc3657debf5a382636f19baadc229e53a7
wangweiya <19309695378@163.com>
2023-03-02 18:21:44 +0800

    123

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,25 +1,20 @@
 <template>
-  <el-table :data="tableData" style="width: 100%"  highlight-current-row :header-cell-style="{ background: '#F7FBFE', color: '#000' }">
+  <el-table :data="tableData" style="width: 100%" :header-cell-style="{ background: '#F7FBFE', color: '#000' }">
     <!-- 复选框 -->
     <el-table-column type="selection" v-if="isTypeSelection" width="55" />
-
     <!-- 表格行 -->
     <el-table-column v-for="item in tableHeader" :prop="item.prop" :label="item.label" :key="item.id">
-    <!-- 建立插槽 -->
+      <!-- 建立插槽 -->
       <template #default="scope">
-    <!-- 按钮 按钮是个数组 -->
+        <!-- 按钮 按钮是个数组 -->
         <div v-if="item.type === 'buttons'">
-    <!-- 循环按钮数组 -->
+          <!-- 循环按钮数组 -->
           <span v-for="(btn, index) in item.buttons" :key="index">
-    <!-- 阅卷按钮，最后按钮判断 -->
+            <!-- 阅卷按钮，最后按钮判断 -->
             <span v-if="btn.text=='exam'">
                  <el-button link :type="btn.type"  @click="$emit(btn.event, scope.row)" >{{scope.row.incomplete>0?'阅卷':'查看'}}</el-button> 
             </span>
-    <!-- 板块表头跳转 -->
-            <span v-else-if="btn.text=='title'">
-                 <el-button link :type="btn.type"  @click="$emit(btn.event, scope.row)"  ><span v-html="scope.row.title"></span></el-button> 
-            </span>
-    <!-- 利用子传递父亲，做按钮点击事件处理 text为按钮的文本 type是按钮的类型 -->
+            <!-- 利用子传递父亲，做按钮点击事件处理 text为按钮的文本 type是按钮的类型 -->
             <el-button v-else link :type="btn.type" size="small" @click="$emit(btn.event, scope.row)" >{{ btn.text }}</el-button>  
           </span>
         </div>
@@ -27,6 +22,12 @@
         <div v-if="item.label=='开放时间'">
           <span>{{scope.row.endtime==null? '不限':scope.row.addtime-scope.row.endtime}}</span>
         </div>
+        <!-- 阅卷详情之班级名称 -->
+        <!-- <div v-if="item.label=='班级名称'">
+            <span>
+              {{scope.row.classname==''? '暂无数据':scope.row.classname}}
+            </span>
+        </div> -->
         <!-- 颜色更改(阅卷未判人数) -->
         <div v-if="item.label=='未判人数'">
             <span v-if="scope.row.incomplete>0" style="color:red" >
